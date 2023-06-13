4f70092a68667b726af712e197ad5378d845fc84
Kal Zheng <kal.zheng@inspire-dt.com>
2023-06-13 14:34:15 +0800

    use camera feature but expo-image-picker has permission issue so i use expo-camera to take the picture

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,21 +1,130 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { useState, useRef } from "react";
+import {
+  Button,
+  View,
+  Alert,
+  TouchableOpacity,
+  Text,
+  Image,
+  StyleSheet,
+} from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+  MediaTypeOptions,
+} from "expo-image-picker";
+import { Camera } from "expo-camera";
+import { Colors } from "../../constants/colors";
 
 function ImagePicker() {
-  async function takeImageHandler() {
-    const image = await launchCameraAsync({
-      allowsEditing: true,
-      aspect: [16, 9],
-      quality: 0.5,
-    });
-    console.log(image);
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef(null);
+  const [pickedImage, setPickedImage] = useState();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      //   const permissionResponse = await requestPermission();
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      //   return permissionResponse.granted;
+      return status;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permission",
+        "You need to grant camera permissions to use this app."
+      );
+
+      return false;
+    }
+    return true;
   }
+
+  //   async function takeImageHandler() {
+  //     const hasPermission = await verifyPermissions();
+
+  //     if (!hasPermission) {
+  //       return;
+  //     }
+
+  //     const image = await launchCameraAsync({
+  //       mediaTypes: MediaTypeOptions.All,
+  //       allowsEditing: true,
+  //       aspect: [16, 9],
+  //       quality: 0.5,
+  //     });
+  //     // console.log(image);
+  //   }
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      //   console.log(data.uri);
+      setPickedImage(data.uri);
+    }
+  };
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+
+      <Camera style={{ flex: 1 }} type={type} ref={cameraRef}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: "flex-end",
+              alignItems: "center",
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <Button title="Take Image" onPress={takePicture} />
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
